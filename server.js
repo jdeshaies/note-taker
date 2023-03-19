2f9ad4cc60d6732190dcc85675431631366c51c8
jd479800 <jd479800@gmail.com>
2023-03-19 14:48:08 -0400

    Adds uuid and fs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
