eefca284d8c2cc4938bc797953529b3e3f37dde5
jd479800 <jd479800@gmail.com>
2023-03-19 19:59:34 -0400

    Retrieves JSON objects in db.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
@@ -13,11 +15,26 @@ app.use(express.static('public'));
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+);
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-)
+);
+
+// // GET Rout for wildcard
+// app.get('*', (req, res) => 
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// );
+
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+// GET Route for retrieving all the notes saved in db.json
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
