6d5fa472dff1bfad4b3a9d1f448c9d3cd83183b6
jd479800 <jd479800@gmail.com>
2023-03-22 18:18:16 -0400

    Updates constants

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,14 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
 
 // GET Route for retrieving all of the notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-  
+  module.exports = notes;
